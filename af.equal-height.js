a1764c22eec5d1fc79ea2dde369660911e632fed
eveevans <eveevans@ubuntu-hdx16.(none)>
2013-04-12 17:09:54 -0600

    definicion de refrencia por defecto y fixes

--- af.equal-height.js
+++ af.equal-height.js
@@ -1,49 +1,43 @@
 // Artfinder Equal Height Children plugin
-//      https://github.com/artfinder/jquery-equal-height
-//      version 1.0
-
+// https://github.com/artfinder/jquery-equal-height
+// mod @eveevans
+// https://github.com/eveevans/jquery-equal-height
+// version 1.1
 
 (function($) {
-    
-    $.fn.make_children_equal_height = function () {
-        var make_equal_height = function (element) {
-            var $within     = $(element),
-                selector    = $within.attr('data-equal-height'),
-                $children   = $(selector, $within),
-                tallest     = 0;
-            
-            $children.each( function() {
-                var $this = $(this);
-                
-                $this.css('min-height', ''); 
-                var h = $this.height();
-                if ( h > tallest ) {
-                    tallest = h;
-                }
-            });
-            
-            $children.each( function() {
-                $(this).css('min-height', tallest + 'px'); 
-            });
-        };
-        
-        return this.each( function () {
-            make_equal_height(this);
-        });
-    };
-	
-	$.fn.make_children_disequal_height = function () {
-		var make_disequal_height = function (element) {
-			var $within     = $(element),
-				selector    = $within.attr('data-equal-height'),
-				$children   = $(selector, $within);
-				$children.each( function() {function () {
-					$(this).css('min-height', '');
-				}};
-		};
-		return this.each( function () {
-			make_disequal_height(this);
-		});
-	};
-    
+
+  $.fn.make_children_equal_height = function () {
+          var make_equal_height = function (element) {            
+              var $within = $(element),               
+                  el = $within.attr('data-equal-height'), /* caso especial en el que se especifica otro nivel para ajustar alto*/
+                  selector = typeof(el) != 'undefined' ? el : '> div',
+                  $children = $(selector, $within),
+                  tallest = 0;                  
+              
+              $children.each( function() {
+                  var $this = $(this);
+                  
+                  $this.css('min-height', '');
+                  var h = $this.height();
+                  if ( h > tallest ) {
+                      tallest = h;
+                  }
+              });
+
+              /* en el caso especial en el que las imagenes deben ser ajustadas*/
+              if (typeof(el) != 'undefined'){
+                $children.find('img').css({'position':'absolute','bottom':'0' });
+              }
+              
+              $children.each( function() {
+                  // $(this).css('height', tallest + 'px');
+                  $(this).animate({'height':tallest});
+              });
+          };
+          
+          return this.each( function () {
+              make_equal_height(this);
+          });
+      };
+
 })( jQuery );
